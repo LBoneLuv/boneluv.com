70006dc970c172055a72776d613ed99d2c2b5657
Jose Manuel Ramirez <boneluvco@gmail.com>
2019-10-04 11:44:00 +0200

    Add services pages

--- src/pages/submited.js
+++ src/pages/submited.js
@@ -8,6 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRocket,faUsers } from '@fortawesome/free-solid-svg-icons'
 import { faArrowAltCircleRight,faFileAlt } from "@fortawesome/free-regular-svg-icons"
 
+require("./main.css")
+
 export default () => (
     <div>
         <Header/>
