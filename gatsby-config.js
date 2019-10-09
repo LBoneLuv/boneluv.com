2eb46aa78462afd32cbb9c31d51d4a6b456fd89a
Jose Manuel Ramirez <boneluvco@gmail.com>
2019-10-09 11:25:54 +0200

    Adding Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,13 @@ module.exports = {
             }
           })
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-102351162-2",
+      },
     }
   ]
 }
